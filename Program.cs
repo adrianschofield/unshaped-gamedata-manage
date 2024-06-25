39fed5c3b97374b9181a56cf1858f29a4d0c3d93
Adrian Schofield <adrianschofield@hotmail.co.uk>
2024-06-25 15:06:36 +0100

    Added support for Okta OIDC Authentication

--- Program.cs
+++ Program.cs
@@ -1,4 +1,36 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Okta OIDC requirements
+builder.Services.AddAuthentication(options =>
+{
+  options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+  options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+})
+.AddCookie()
+.AddOpenIdConnect(options =>
+{
+  options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+  options.Authority = builder.Configuration["Okta:Domain"] + "/oauth2/default";
+  options.RequireHttpsMetadata = true;
+  options.ClientId = builder.Configuration["Okta:ClientId"];
+  options.ClientSecret = builder.Configuration["Okta:ClientSecret"];
+  options.ResponseType = OpenIdConnectResponseType.Code;
+  options.GetClaimsFromUserInfoEndpoint = true;
+  options.Scope.Add("openid");
+  options.Scope.Add("profile");
+  options.SaveTokens = true;
+  options.TokenValidationParameters = new TokenValidationParameters
+  {
+    NameClaimType = "name",
+    RoleClaimType = "groups",
+    ValidateIssuer = true
+  };
+});
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -18,6 +50,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
