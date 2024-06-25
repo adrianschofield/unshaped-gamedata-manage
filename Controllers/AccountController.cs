26f989d711c001125f25a615675f55f8b0906542
Adrian Schofield <adrianschofield@hotmail.co.uk>
2024-06-25 15:08:34 +0100

    Add the controller to support Login

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Mvc;
+
+namespace unshaped_gamedata_manage.Controllers;
+  public class AccountController : Controller
+  {
+    public IActionResult Login()
+    {
+      if(!HttpContext.User.Identity.IsAuthenticated)
+      {
+        return Challenge(OpenIdConnectDefaults.AuthenticationScheme);
+      }
+      return RedirectToAction("Index", "Home");
+    }
+
+    public IActionResult Logout()
+    {
+      return new SignOutResult(new[]
+      {
+        OpenIdConnectDefaults.AuthenticationScheme,
+        CookieAuthenticationDefaults.AuthenticationScheme
+      });
+    }
+  }
