fea9d7539f42943b231d10a5d72987dea6279d8f
Adrian Schofield <adrianschofield@hotmail.co.uk>
2024-06-25 10:39:29 +0100

    Initial commit for skeleton MVC app

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace unshaped_gamedata_manage.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
